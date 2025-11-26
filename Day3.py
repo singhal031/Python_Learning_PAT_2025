0d98a947e9613ec9e1af23cd097690d4e2bcf15b
singhal031 <naman.singhal@cognizant.com>
2025-11-26 19:36:07 +0530

    Create Day3.py

---
+++ Day3.py
@@ -0,0 +1,15 @@
+# Take user input
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+city = input("Enter your city: ")
+print(f"Hello, {name}! I'm {age} years old and live in {city}.")
+
+
+
+# Get user input
+user_input = input("Enter a phrase: ")
+formatted_sentence = user_input.strip().capitalize()
+if not format_sentence.endswith('.'):
+    format_sentence += '.'
+
+print(format_sentence)
